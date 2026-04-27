6eb3d0a026d0a4adf123bd8a1364f43b383e3fca
aybouatr <ayoubleet@gmail.com>
2026-04-27 15:39:16 +0100

    add new example

--- EventConsoleAppFromScratc/Program.cs
+++ EventConsoleAppFromScratc/Program.cs
@@ -109,35 +109,138 @@ namespace EventConsoleAppFromScratc
         }
     }
 
+
+    // Employee Attendance and Departure
+    // (using Events)
+    // When an employee records their arrival or departure,
+    // the registration device triggers an Event
+    // that notifies three departments:
+    // Payroll (records the time),
+    // Security (alerts if the employee leaves before 2:00 PM),
+    // and Human Resources (calculates lateness after 8:00 AM).
+
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+
+        public Employee(int id, string name, DateTime arrivalTime, DateTime departureTime)
+        {
+            Id = id;
+            Name = name;
+            ArrivalTime = arrivalTime;
+            DepartureTime = departureTime;
+        }
+    }
+
+
+    public class  MarkDay
+    {
+        public DateTime DateCome { get; set; }
+
+        public DateTime DateLeave { get; set; }
+        public MarkDay( DateTime dateCome, DateTime dateLeave)
+            {
+                DateCome = dateCome;
+                DateLeave = dateLeave;
+            }
+
+    }
+
+    public class EventMarkDay
+    {
+        
+        public event EventHandler<Employee>? OnDayMarked;
+
+
+        public void MarkDay(string Name,DateTime arrivalTime, DateTime leavetime)
+        {
+            Employee em = new Employee(0, Name, arrivalTime, leavetime);
+
+            RaiseEvent(em);
+        }
+
+        protected virtual void RaiseEvent(Employee Date)
+        {
+                OnDayMarked?.Invoke(this, Date);
+        }
+
+    }
+
+
+    public class PayrollDepartment
+    {
+        private static readonly TimeSpan StartTime = new(8, 0, 0);
+        private static readonly TimeSpan EndTime = new(17, 0, 0); // 5:00 PM
+
+        public void Subscribe(EventMarkDay eventMarkDay)
+        {
+            eventMarkDay.OnDayMarked += RecordTime;
+        }
+
+        private void RecordTime(object? sender, Employee employee)
+        {
+            if (employee.ArrivalTime.TimeOfDay < StartTime)
+            {
+                Console.WriteLine($"Employee arrived before 8:00 AM: {employee.ArrivalTime}");
+            }
+
+            if (employee.DepartureTime.TimeOfDay > EndTime)
+            {
+                Console.WriteLine($"Employee left after 5:00 PM: {employee.DepartureTime}");
+            }
+        }
+    }
+
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-         
 
-            NewsPuplisher newsPublisher = new NewsPuplisher();
-            NewsSubscriber newsSubscriber = new NewsSubscriber("Subscriber1");
-            NewsSubscriber newsSubscriber1 = new NewsSubscriber("Subscriber2");
-            NewsSubscriber newsSubscriber2 = new NewsSubscriber("Subscriber3");
-
-
-            newsSubscriber.Subscribe(newsPublisher);
-            newsSubscriber1.Subscribe(newsPublisher);
-            newsSubscriber2.Subscribe(newsPublisher);
+            //{
+            //    NewsPuplisher newsPublisher = new NewsPuplisher();
+            //    NewsSubscriber newsSubscriber = new NewsSubscriber("Subscriber1");
+            //    NewsSubscriber newsSubscriber1 = new NewsSubscriber("Subscriber2");
+            //    NewsSubscriber newsSubscriber2 = new NewsSubscriber("Subscriber3");
 
 
+            //    newsSubscriber.Subscribe(newsPublisher);
+            //    newsSubscriber1.Subscribe(newsPublisher);
+            //    newsSubscriber2.Subscribe(newsPublisher);
 
-            newsPublisher.CreateArticle("Breaking News", "This is the content of the breaking news article.");
-          
-            Console.WriteLine("--------------------------------------------------------------------------------");
-            newsSubscriber.Unsubscribe(newsPublisher);
-            newsPublisher.CreateArticle("Another News", "This is the content of another news article.");
+
+
+            //    newsPublisher.CreateArticle("Breaking News", "This is the content of the breaking news article.");
+
+            //    Console.WriteLine("--------------------------------------------------------------------------------");
+            //    newsSubscriber.Unsubscribe(newsPublisher);
+            //    newsPublisher.CreateArticle("Another News", "This is the content of another news article.");
+
+            //}
+
+
+
+            EventMarkDay eventMarkDay = new EventMarkDay();
+
+            PayrollDepartment payrollDepartment = new PayrollDepartment();
+
+            payrollDepartment.Subscribe(eventMarkDay);
+
+            eventMarkDay.MarkDay("John Doe", DateTime.Now, DateTime.Now.AddHours(8));
+            eventMarkDay.MarkDay("Jane Smith", DateTime.Now.AddHours(-1), DateTime.Now.AddHours(7));
+            eventMarkDay.MarkDay("Bob Johnson", DateTime.Now.AddHours(1), DateTime.Now.AddHours(9));
+
 
 
             Console.ReadLine();
-            
-
-
         }
     }
 }
