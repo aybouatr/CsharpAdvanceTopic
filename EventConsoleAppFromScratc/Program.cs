40097c95c48e1b38224cd3ab1976956b8735fa2f
aybouatr <ayoubleet@gmail.com>
2026-04-27 13:33:02 +0100

    add

--- EventConsoleAppFromScratc/Program.cs
+++ EventConsoleAppFromScratc/Program.cs
@@ -3,55 +3,109 @@
 namespace EventConsoleAppFromScratc
 {
 
-    public class SumArguments : EventArgs
+    //public class SumArguments : EventArgs
+    //{
+    //    public int A { get;}
+    //    public int B { get; }
+
+    //    public int SumAAndBC { get; }
+
+
+    //   public SumArguments(int a, int b)
+    //    {
+    //        A = a;
+    //        B = b;
+    //        SumAAndBC = a + b;
+    //    }
+    //}
+
+    //public class MakeEvent
+    //{
+    //    public int A { get; }
+
+    //    public int B { get; }
+
+    //    public event EventHandler<SumArguments>? OnSumEvent;
+
+
+    //    public void RasieEvent(int A, int B)
+    //    {
+    //        RasieEvent(new SumArguments(A, B));
+    //    }
+
+    //    protected virtual void RasieEvent(SumArguments args)
+    //    {
+    //        OnSumEvent?.Invoke(this, args);
+    //    }
+
+    //}
+
+
+    //public class Displa
+    //{
+
+    //    public void Subscribe(MakeEvent makeEvent)
+    //    {
+    //        makeEvent.OnSumEvent += DisplaySum;
+    //    }
+
+    //    public void DisplaySum(object? sender, SumArguments args)
+    //    {
+    //        Console.WriteLine($"The sum of {args.A} and {args.B} is {args.SumAAndBC}");
+    //    }
+    //}
+
+
+    public class NewArticts
     {
-        public int A { get;}
-        public int B { get; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
 
-        public int SumAAndBC { get; }
-
-
-       public SumArguments(int a, int b)
+       public NewArticts(string title, string content   )
         {
-            A = a;
-            B = b;
-            SumAAndBC = a + b;
+            Title = title;
+            Content = content;
         }
     }
 
-    public class MakeEvent
+
+    class NewsPuplisher
     {
-        public int A { get; }
-
-        public int B { get; }
-
-        public event EventHandler<SumArguments>? OnSumEvent;
-
-
-        public void RasieEvent(int A, int B)
+        public event EventHandler<NewArticts>? OnPersonCreated;
+        public void CreateArticle(string title, string content)
         {
-            RasieEvent(new SumArguments(A, B));
+            NewArticts article = new NewArticts(title, content);
+
+            RaiseEvent(article);
         }
-
-        protected virtual void RasieEvent(SumArguments args)
+        protected virtual void RaiseEvent(NewArticts article)
         {
-            OnSumEvent?.Invoke(this, args);
+            OnPersonCreated?.Invoke(this, article);
         }
 
     }
 
-
-    public class Displa
+    class NewsSubscriber    
     {
+        string ? Name { get; set; }
 
-        public void Subscribe(MakeEvent makeEvent)
+
+        public NewsSubscriber(string name)
         {
-            makeEvent.OnSumEvent += DisplaySum;
+            Name = name;
+        }
+        public void Subscribe(NewsPuplisher eventMake)
+        {
+            eventMake.OnPersonCreated += Display;
         }
 
-        public void DisplaySum(object? sender, SumArguments args)
+        public void Unsubscribe(NewsPuplisher eventMake)
         {
-            Console.WriteLine($"The sum of {args.A} and {args.B} is {args.SumAAndBC}");
+            eventMake.OnPersonCreated -= Display;
+        }
+        private void Display(object? sender, NewArticts person)
+        {
+           Console.WriteLine($"Name Subscriber: {Name}, Title: {person.Title}, Content: {person.Content}");
         }
     }
 
@@ -59,12 +113,31 @@ namespace EventConsoleAppFromScratc
     {
         static void Main(string[] args)
         {
-            MakeEvent makeEvent = new MakeEvent();
-            Displa displa = new Displa();
+         
 
-            displa.Subscribe(makeEvent);
+            NewsPuplisher newsPublisher = new NewsPuplisher();
+            NewsSubscriber newsSubscriber = new NewsSubscriber("Subscriber1");
+            NewsSubscriber newsSubscriber1 = new NewsSubscriber("Subscriber2");
+            NewsSubscriber newsSubscriber2 = new NewsSubscriber("Subscriber3");
 
-            makeEvent.RasieEvent(5, -10);
+
+            newsSubscriber.Subscribe(newsPublisher);
+            newsSubscriber1.Subscribe(newsPublisher);
+            newsSubscriber2.Subscribe(newsPublisher);
+
+
+
+            newsPublisher.CreateArticle("Breaking News", "This is the content of the breaking news article.");
+          
+            Console.WriteLine("--------------------------------------------------------------------------------");
+            newsSubscriber.Unsubscribe(newsPublisher);
+            newsPublisher.CreateArticle("Another News", "This is the content of another news article.");
+
+
+            Console.ReadLine();
+            
+
+
         }
     }
 }
