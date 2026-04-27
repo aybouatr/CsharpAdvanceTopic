1e88cdd18d3f65e5ca381d802aabeb33d36c907a
aybouatr <ayoubleet@gmail.com>
2026-04-27 11:15:37 +0100

    simple example how build Event

---
+++ EventConsoleAppFromScratc/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace EventConsoleAppFromScratc
+{
+
+    public class SumArguments : EventArgs
+    {
+        public int A { get;}
+        public int B { get; }
+
+        public int SumAAndBC { get; }
+
+
+       public SumArguments(int a, int b)
+        {
+            A = a;
+            B = b;
+            SumAAndBC = a + b;
+        }
+    }
+
+    public class MakeEvent
+    {
+        public int A { get; }
+
+        public int B { get; }
+
+        public event EventHandler<SumArguments>? OnSumEvent;
+
+
+        public void RasieEvent(int A, int B)
+        {
+            RasieEvent(new SumArguments(A, B));
+        }
+
+        protected virtual void RasieEvent(SumArguments args)
+        {
+            OnSumEvent?.Invoke(this, args);
+        }
+
+    }
+
+
+    public class Displa
+    {
+
+        public void Subscribe(MakeEvent makeEvent)
+        {
+            makeEvent.OnSumEvent += DisplaySum;
+        }
+
+        public void DisplaySum(object? sender, SumArguments args)
+        {
+            Console.WriteLine($"The sum of {args.A} and {args.B} is {args.SumAAndBC}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MakeEvent makeEvent = new MakeEvent();
+            Displa displa = new Displa();
+
+            displa.Subscribe(makeEvent);
+
+            makeEvent.RasieEvent(5, -10);
+        }
+    }
+}
