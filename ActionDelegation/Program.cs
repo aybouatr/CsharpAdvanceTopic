bab4fabacc833564c9153799bdef76cc91cc7e09
aybouatr <ayoubleet@gmail.com>
2026-04-30 16:06:41 +0100

    add delgation func and action

---
+++ ActionDelegation/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ActionDelegation
+{
+    internal class Program
+    {
+
+            static public Action<string> action = Method1;
+            static public Action<string> action2 = Method2;
+    
+            public static void Method1(string message)
+            {
+                Console.WriteLine("Method1: " + message);
+            }
+
+            public static void Method2(string message)
+            {
+                Console.WriteLine("Method2: " + message);
+            }
+
+        static void Main(string[] args)
+        {
+            action("Hello, Action Delegation!");
+            action2("Hello, Action Delegation 2!");
+        }
+    }
+}
---
+++ FuncDelegation/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FuncDelegation
+{
+    internal class Program
+    {
+
+        static public Func<string> func = Method1;
+
+        static public Action<string> func2 = Method2;
+
+        public static string Method1()
+        {
+            return "Method1: Hello, World!";
+        }
+
+        public static void Method2(string message)
+        {
+
+            Console.WriteLine("Method2: " + message);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(func());
+            func2("Hello, Func Delegation!");
+        }
+    }
+}
