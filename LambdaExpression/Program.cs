b13e44a81fb3246d3048b938898ea4bca32057bd
aybouatr <ayoubleet@gmail.com>
2026-04-30 18:44:54 +0100

    add

---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LambdaExpression
+{
+    internal class Program
+    {
+        // expresion lambda
+
+            static public Func<int, int> square = x => x * x;
+    
+            static public Func<int, int, int> add = (x, y) => x + y;
+    
+            static public Action<string> printMessage = message => Console.WriteLine(message);
+
+        static void Main(string[] args)
+        {
+
+            printMessage("Hello, Lambda Expressions!");
+            Console.WriteLine("Square of 5: " + square(5));
+            Console.WriteLine("Sum of 3 and 4: " + add(3, 4));
+
+        }
+    }
+}
---
+++ using/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using static System.Math;
+
+
+using hh = System.Console;// using for aliasing
+
+namespace UsingExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            hh.WriteLine("Using Example this result squre for 16 = {0}", Sqrt(16));
+            hh.WriteLine("Hello, World!");
+        }
+    }
+}
