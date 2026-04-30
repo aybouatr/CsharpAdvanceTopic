c5c0cff4e7f9fddc3e334a8bc75ce133d384bbd6
aybouatr <ayoubleet@gmail.com>
2026-04-30 16:31:22 +0100

    add all delagation types

---
+++ PredicateDelegate/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PredicateDelegate
+{
+    internal class Program
+    {
+        static Predicate<int> PrDelagate = IsEven;
+
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PrDelagate(0));
+            Console.WriteLine(PrDelagate(91));
+        }
+    }
+}
