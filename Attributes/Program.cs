8d3e92c00a56339aee06136574441001bd8deee1
aybouatr <ayoubleet@gmail.com>
2026-05-01 17:28:28 +0100

    add attribute examples

---
+++ Attributes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace Attributes
+{
+    internal class Program
+    {
+
+        public class MyAttribute
+        {
+        
+
+            [Conditional("DEBUG")]
+            public void DEBUGMethod()
+            {
+                Console.WriteLine("This is a DEBUG method in MyAttribute.");
+            }
+
+            public void NormalMethod()
+            {
+                Console.WriteLine("This is a normal method in MyAttribute.");
+            }
+
+          
+        }
+
+
+        static void Main(string[] args)
+        {
+            MyAttribute myAttr = new MyAttribute();
+
+            //Console.WriteLine($"Name: {myAttr.Name}, Value: {myAttr.Value}");
+
+            myAttr.DEBUGMethod();
+            myAttr.NormalMethod();
+
+
+        }
+
+    }
+}
