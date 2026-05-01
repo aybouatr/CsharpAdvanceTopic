3da4bd32920de79943ceff1bf82e87239bddccf4
aybouatr <ayoubleet@gmail.com>
2026-05-01 15:44:39 +0100

    add

--- SerializationExample/Program.cs
+++ SerializationExample/Program.cs
@@ -1,40 +1,72 @@
-﻿using System.Runtime.InteropServices.JavaScript;
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text.Json;
+using System.Xml.Serialization;
 
 namespace SerializationExample
 {
 
-
+    [Serializable]
     public class Person
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int Age { get; set; }
+        public DateTime Birth { get; set; }
+        public string Ema { get; set; } = string.Empty;
 
-        public DateOnly Birth { get; set; }
+        public Person()
+        {
+        }
 
-        public string Ema { get; set; }
-
-
-        public Person(string name, int age, DateOnly birth, string ema)
+        public Person(string name, int age, DateTime birth, string ema)
         {
             Name = name;
             Age = age;
             Birth = birth;
             Ema = ema;
         }
-
-
     }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Person person = new("John Doe", 30, new DateTime(1994, 5, 15), "example@gmail.com");
 
-            Person person = new Person("John Doe", 30, new DateOnly(1994, 5, 15), "example@gmail.com");
-        
-            string json = System.Text.Json.JsonSerializer.Serialize(person);
+            string json = JsonSerializer.Serialize(person);
 
-            Console.WriteLine(json);
+
+
+            //XmlSerializer ser = new XmlSerializer(typeof(Person));
+
+            //using (TextWriter sw = new StreamWriter("example.xml"))
+            //{
+            //    ser.Serialize(sw, person);
+
+            //}
+
+
+            //using (TextReader sr = new StreamReader("example.xml"))
+            //{
+            //    Person deserializedPerson = (Person)ser.Deserialize(sr);
+            //    Console.WriteLine($"Name: {deserializedPerson.Name}, Age: {deserializedPerson.Age}, Birth: {deserializedPerson.Birth}, Email: {deserializedPerson.Ema}");
+
+            //}
+
+
+
+            DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(Person));
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                jsonSerializer.WriteObject(ms, person);
+                ms.Position = 0;
+                StreamReader sr = new StreamReader(ms);
+                string jsonData = sr.ReadToEnd();
+                File.WriteAllText("example.json", jsonData);
+
+            }
+
         }
-                
     }
 }
