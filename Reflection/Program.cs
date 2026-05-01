26cfe54e35ca94e79e0f5e040e5e591fef3043f4
aybouatr <ayoubleet@gmail.com>
2026-05-01 21:41:40 +0100

    add Redlection Example

---
+++ Reflection/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Reflection
+{
+
+    //public class MyClass
+    //{
+    //    public string Name { get; set; }
+
+    //    public int Id { get; set; }
+
+    //    public void MyMethod()
+    //    {
+    //        Console.WriteLine("This is a method in MyClass.");
+    //    }
+
+    //    public MyClass(string name, int id) 
+        
+    //    {
+    //         Name = name;
+    //        Id = id;
+    //    }
+    //}
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Type KnowType = typeof(string);
+
+
+            Console.WriteLine("this is Full Name : {0}",KnowType.FullName);
+            Console.WriteLine("this is Name : {0}",KnowType.Name);
+            Console.WriteLine("this is Namespace : {0}",KnowType.Namespace);
+
+        }
+    }
+}
