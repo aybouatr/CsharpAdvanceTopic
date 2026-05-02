f8ed208a23f47f1d1f094faba5117e4092ec0d8d
aybouatr <ayoubleet@gmail.com>
2026-05-02 21:20:25 +0100

    add example custume attribute

---
+++ ReflectionwithCustomAttributes/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ReflectionwithCustomAttributes
+{
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+    public class MyCustomAttribute : Attribute
+    {
+        public string Description { get; }
+
+        public MyCustomAttribute(string description)
+        {
+            Description = description;
+        }
+
+    }
+
+    [MyCustomAttribute("This is a custom attribute for the MyClass class")]
+   public class MyClass
+   {
+        [MyCustomAttribute("This is a custom method attribute")]
+        public void MyMethod()
+        {
+            Console.WriteLine("Executing MyMethod");
+        }
+   }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type myClassType = typeof(MyClass);
+
+            
+            Object[] classAttributes = myClassType.GetCustomAttributes(typeof(MyCustomAttribute), false);
+
+            foreach (MyCustomAttribute attr in classAttributes)
+            {
+                Console.WriteLine($"Class Attribute Description: {attr.Description}");
+            }
+            // Get method information
+
+            Console.WriteLine("\n--- Method Attributes ---");
+
+            var methods = myClassType.GetMethods();
+
+            foreach (var method in methods)
+            {
+                var methodAttributes = method.GetCustomAttributes(typeof(MyCustomAttribute), false);
+                foreach (MyCustomAttribute attr in methodAttributes)
+                {
+                    Console.WriteLine($"\nMethod Attribute Description: {attr.Description}");
+                }
+            }
+        }
+    }
+}
