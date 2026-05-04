58c25ab48b87501ffa8b02b432f88f7ff1cc5e84
aybouatr <ayoubleet@gmail.com>
2026-05-04 12:12:00 +0100

    add String Builder

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+
+
+    internal class Program
+    {
+        public static void DemonstrateStringBuilder(int iterations)
+        {
+             StringBuilder sb = new StringBuilder();
+            while (iterations > 0)
+            {
+                sb.Append("Hello, World! ");
+                iterations--;
+            }
+        }
+
+        public static void DemonstrateStringNormal(int iterations)
+        {
+            string result = "";
+            while (iterations > 0)
+            {
+                result += "Hello, World! ";
+                iterations--;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            DemonstrateStringBuilder(20000);
+            stopwatch.Stop();
+
+            Console.WriteLine("StringBuilder time: " + stopwatch.ElapsedMilliseconds + " ms");
+
+            Stopwatch stopwatch2 = new Stopwatch();
+
+            stopwatch2.Start();
+            DemonstrateStringNormal(20000);
+            stopwatch2.Stop();
+
+            Console.WriteLine("String concatenation time: " + stopwatch2.ElapsedMilliseconds + " ms");
+        }
+    }
+
